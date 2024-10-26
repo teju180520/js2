e21d494becde8b6aef120f5b905633ac5cb72b7c
teju <laditejeswari912@gmail.com>
2024-10-26 11:30:29 +0530

    mass

---
+++ js.js
@@ -0,0 +1,71 @@
+//arthametic operators
+let a=+prompt("enter your lucky");
+let b=+prompt("add your age")
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a/b);
+console.log(a%b);
+console.log(a**b);
+
+
+//assingment operators
+let c=10;
+c+=10;
+console.log(c);
+let e=10;
+e-=10;
+console.log(e);
+let f=10;
+f*=10;
+console.log(f);
+let g=10;
+g/=11;
+console.log(g);
+
+
+//comparison operators
+let isu=50;
+let aisu=100;
+
+console.log(isu<aisu);
+console.log(isu>aisu);
+console.log(isu!=aisu);
+
+let h=10;
+let j=10;
+console.log(h==j);
+console.log(h<=j);
+console.log(h>=j);
+console.log(h===j);
+console.log(h!=j);
+
+let i="20";
+let k=10;
+console.log(i==k)
+console.log(i===k)
+
+let l="a";
+let m="z";
+console.log(l<m);
+console.log(l>m);
+
+let n="A";
+let o="a";
+console.log(n==o);
+console.log(n<o);
+
+
+//logical operators
+let p=true
+let r=false
+
+console.log(p && p)
+console.log(p && r)
+console.log(r && p)
+console.log(r && r)
+
+console.log(p || p)
+console.log(p || r)
+console.log(r || p)
+console.log(r || r)
